77ad837b91fba485008cd743b577714117d0aefe
Alexandru Constantin <alex@devmotion.ro>
2019-04-15 12:04:43 +0300

    custom exception & destroy handling

--- digistorage.py
+++ digistorage.py
@@ -8,6 +8,9 @@ import yaml
 API_BASE = 'https://storage.rcs-rds.ro'
 
 
+class DigiStorageApiException(Exception):
+    pass
+
 class DigiStorageApi:
     def __init__(self, mount_name='Digi Cloud', path_to_config='config.yaml', email=None, password=None):
         """
@@ -35,7 +38,7 @@ class DigiStorageApi:
         }
         r = requests.get(API_BASE + '/token', headers=headers)
         if r.status_code != 200:
-            raise Exception('Error authenticating. Wrong username/password?')
+            raise DigiStorageApiException('Error authenticating. Wrong username/password?')
 
         return r.headers['X-Koofr-Token']
 
@@ -45,7 +48,7 @@ class DigiStorageApi:
     def __get_mount_by_name(self, mount_name):
         found_mounts_with_name = [m for m in self.mounts if m['name'] == mount_name]
         if not found_mounts_with_name:
-            raise Exception('No mount has been found under the name: ' + mount_name)
+            raise DigiStorageApiException('No mount has been found under the name: ' + mount_name)
         return found_mounts_with_name[0]
 
     def upload(self, path, remote_path):
@@ -53,7 +56,7 @@ class DigiStorageApi:
         Convenience method: uploads either a file or a folder
         :param path: path on disk to the file/folder to upload, with no trailing /
         :param remote_path: path to upload to, WITHOUT the file/folder name, with no / at the beginning
-        :return: None (will raise exception on error)
+        :return: None
         """
         if os.path.isdir(path):
             for file_name in os.listdir(path):
@@ -66,7 +69,7 @@ class DigiStorageApi:
         :param file_path: path on disk to the file to upload
         :param remote_path: path to upload to, WITHOUT the file name, with no / at the beginning
         :param remote_file_name: name of the file to upload to, defaults to the file name
-        :return: response object (will raise exception on error)
+        :return: response object
         """
         if remote_file_name is None:
             remote_file_name = os.path.basename(file_path)
@@ -77,14 +80,14 @@ class DigiStorageApi:
                                   )
             f.close()
             if r.status_code != 200:
-                raise Exception('Error uploading file: ' + file_path)
+                raise DigiStorageApiException('Error uploading file: ' + file_path)
             return r
 
     def upload_files(self, file_paths, remote_path):
         """
         :param file_paths: array of paths on disk to files
         :param remote_path: path to upload to, WITHOUT the file name, with no / at the beginning
-        :return: None (will raise exception on error)
+        :return: None
         """
         for file_path in file_paths:
             self.upload_file(file_path, remote_path)
@@ -92,7 +95,7 @@ class DigiStorageApi:
     def create_folder(self, remote_path):
         """
         :param remote_path: full path containing the folder name, with no / at the beginning
-        :return: response object (will raise exception on error)
+        :return: response object
         """
         r = self.session.post(API_BASE + '/api/v2/mounts/' + self.mount['id'] + '/files/folder',
                               params={'path': '/'},
@@ -100,19 +103,19 @@ class DigiStorageApi:
                               headers={'content-type': 'application/json'}
                               )
         if r.status_code != 200:
-            raise Exception('Error creating folder: ' + remote_path)
+            raise DigiStorageApiException('Error creating folder: ' + remote_path)
         return r
 
     def remove_file_folder(self, remote_path):
         """
         :param remote_path: full path containing the folder/file name, with no / at the beginning
-        :return: response object (will raise exception on error)
+        :return: response object
         """
         r = self.session.delete(API_BASE + '/api/v2/mounts/' + self.mount['id'] + '/files/remove',
                                 params={'path': '/' + remote_path},
                                 )
         if r.status_code != 200:
-            raise Exception('Error removing file/folder: ' + remote_path)
+            raise DigiStorageApiException('Error removing file/folder: ' + remote_path)
         return r
 
     def file_folder_info(self, remote_path):
@@ -127,6 +130,13 @@ class DigiStorageApi:
         if r.status_code != 200:
             return None
         return json.loads(r.content)
+
+    def destroy(self):
+        """ Make sure to call this method when finished with interacting w/ the API to revoke the auth token """
+        r = self.session.delete(API_BASE + '/token', headers={'Accept': '*/*'})
+        if r.status_code != 204:
+            raise DigiStorageApiException('Error revoking access token')
+        return r
 
 
 if __name__ == '__main__':
@@ -143,13 +153,18 @@ if __name__ == '__main__':
                         help='Remote path to execute the desired action on')
 
     args = parser.parse_args()
-    dsa = DigiStorageApi()
 
-    if args.upload:
-        dsa.upload(args.upload, remote_path=args.remote_path)
-    elif args.info:
-        print(dsa.file_folder_info(args.remote_path))
-    elif args.mkdir:
-        dsa.create_folder(args.remote_path)
-    elif args.rm:
-        dsa.remove_file_folder(args.remote_path)
+    try:
+        dsa = DigiStorageApi()
+        if args.upload:
+            dsa.upload(args.upload, remote_path=args.remote_path)
+        elif args.info:
+            print(dsa.file_folder_info(args.remote_path))
+        elif args.mkdir:
+            dsa.create_folder(args.remote_path)
+        elif args.rm:
+            dsa.remove_file_folder(args.remote_path)
+    except DigiStorageApiException as dsae:
+        raise dsae
+    finally:
+        dsa.destroy()
